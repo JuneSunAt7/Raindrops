7968460c71c946f1676577675b6c55fd8f295dd6
Gem13 <jprogram@bk.ru>
2025-02-24 22:22:32 +0300

    del comments

--- win_client.sh
+++ win_client.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# Определяем массив с исходными файлами
+
 FILES=("key_gens/generate_ssl.go" "cmd/client/client.go" "cmd/server.go" "cmd/data_mgmt/server_mgmt.go" "cmd/data_mgmt/client_mgmt.go" "cmd/data_mgmt/data_mgmt.go" "cmd/intelligence/run_cli_intell.go")
 
-# Определяем массив с целевыми операционными системами
+
 PLATFORMS=("windows" "linux")
 
-# Цикл по платформам
+
 for platform in "${PLATFORMS[@]}"; do
     echo "Building for $platform..."
 
-    # Устанавливаем GOOS в зависимости от платформы
     if [ "$platform" = "windows" ]; then
         GOOS=windows
         EXT=".exe"
@@ -24,12 +23,10 @@ for platform in "${PLATFORMS[@]}"; do
 
     GOARCH=amd64
 
-    # Цикл по файлам
     for file in "${FILES[@]}"; do
-        # Извлекаем имя файла без пути и расширения
+
         filename=$(basename "$file" .go)
-        
-        # Собираем приложение
+
         go build -o "build/$platform/${filename}${EXT}" -ldflags="-s -w" "$file"
         echo "Built: build/$platform/${filename}${EXT}"
     done
