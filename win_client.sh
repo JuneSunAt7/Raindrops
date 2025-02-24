7c00d0efecd6fe6354effb77dc35f6ae8972e8a6
Gem13 <jprogram@bk.ru>
2025-02-24 22:21:34 +0300

    upd script

--- win_client.sh
+++ win_client.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
 
-GOOS=windows GOARCH=amd64 go build -o certs key_gens/generate_ssl.go
+# Определяем массив с исходными файлами
+FILES=("key_gens/generate_ssl.go" "cmd/client/client.go" "cmd/server.go" "cmd/data_mgmt/server_mgmt.go" "cmd/data_mgmt/client_mgmt.go" "cmd/data_mgmt/data_mgmt.go" "cmd/intelligence/run_cli_intell.go")
 
-echo "Press enter to continue";
-read name;
+# Определяем массив с целевыми операционными системами
+PLATFORMS=("windows" "linux")
+
+# Цикл по платформам
+for platform in "${PLATFORMS[@]}"; do
+    echo "Building for $platform..."
+
+    # Устанавливаем GOOS в зависимости от платформы
+    if [ "$platform" = "windows" ]; then
+        GOOS=windows
+        EXT=".exe"
+    elif [ "$platform" = "linux" ]; then
+        GOOS=linux
+        EXT=""
+    else
+        echo "Unsupported platform: $platform"
+        continue
+    fi
+
+    GOARCH=amd64
+
+    # Цикл по файлам
+    for file in "${FILES[@]}"; do
+        # Извлекаем имя файла без пути и расширения
+        filename=$(basename "$file" .go)
+        
+        # Собираем приложение
+        go build -o "build/$platform/${filename}${EXT}" -ldflags="-s -w" "$file"
+        echo "Built: build/$platform/${filename}${EXT}"
+    done
+done
+
+echo "Press enter to continue"
+read name
